6f56dd2f48e74a41f041d62c1397210e22688cb4
kevin <kevin_chaney@icloud.com>
2023-03-07 15:05:40 -0800

    added board size change functionality

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-let board = document.querySelector('.board')
+function populateBoard(size){
+    let board = document.querySelector('.board')
+    //creates a template for a grid of 16x16 with a width of 1 fr
+    board.style.gridTemplateColums = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    //for loop to create square divs inside board
+    for(let i = 0; i < 256; i++){
+        let square = document.createElement('div')
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
 
-//creates a grid of 16x16 with a width of 1 fr
-board.style.gridTemplateColums = 'repeat(16, 1fr)'
-board.style.gridTemplateRows = 'repeat(16, 1fr)'
+populateBoard(16)
 
-for(let i = 0; i < 256; i++){
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue';
-    board.insertAdjacentElement("beforeend", square);
+function changeSize(input){
+    populateBoard(input)
 }
