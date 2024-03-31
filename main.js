6b70d487b69290c01abd4f2ca2b74191c10684cb
kevin <kevin_chaney@icloud.com>
2024-03-30 21:10:50 -0700

    Finished the project via youtube tutorial. Spent too much time on this and want to move on to new projects/refreshing things that were already learned. Sorry programmers

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-function populateBoard(size){
-    let board = document.querySelector('.board')
+let color = 'black';
+
+
+
+function populateBoard(size) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
     //creates a template for a grid of 16x16 with a width of 1 fr
-    board.style.gridTemplateColums = `repeat(${size}, 1fr)`
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    //for loop to create square divs inside board
-    for(let i = 0; i < 256; i++){
-        let square = document.createElement('div')
-        square.style.backgroundColor = 'blue';
+//for loop to create square divs inside board
+    let amount = size * size;
+    for(let i = 0; i < amount; i++){
+        let square = document.createElement("div");
+        square.addEventListener('mouseover', colorSquare)
+        square.style.backgroundColor = "white"
         board.insertAdjacentElement("beforeend", square);
     }
 }
 
-populateBoard(16)
+populateBoard(16);
 
-function changeSize(input){
-    populateBoard(input)
+function changeSize(input) {
+    if ( input >= 2 && input <= 100 ) {
+        populateBoard(input);
+    } else {
+        console.log("too many squares");
+    }
+}
+
+function colorSquare() {
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }else{
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice){
+    color = choice;
 }
